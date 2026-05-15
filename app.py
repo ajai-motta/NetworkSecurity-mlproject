6a5b5618dbe567f4b5b3ed24b0c94fe25cce1a18
Ajai Joseph <ajayjoseo8@gmail.com>
2026-05-15 13:11:39 +0530

    uvicon, fastapi jinga2

---
+++ app.py
@@ -0,0 +1,66 @@
+import sys,os
+import certifi
+import psycopg2
+from dotenv import load_dotenv
+from networksecurity.logging.logging import logging
+from networksecurity.exception.exception import CustomException
+from networksecurity.utils.read_write import load_object_pkl
+from networksecurity.utils.sql_related import connect_to_postgres
+load_dotenv()
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI,File,UploadFile,Request
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
+from uvicorn import run as app_run
+import pandas as pd
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="templates")
+
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_port = os.getenv("DB_PORT")
+
+# connect to the PostgreSQL database
+
+conn=connect_to_postgres()
+app=FastAPI()
+origins=["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/",tags=["Authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.post("/predict",tags=["Prediction"])
+async def predict(request:Request,file:UploadFile=File(...)):
+    try:
+        #contents=await file.read()
+        #df=pd.read_csv(StringIO(contents.decode("utf-8")))
+        df=pd.read_csv(file.file)
+        model=load_object_pkl("final_model/final_model.pkl")
+        preprocessor=load_object_pkl("final_model/final_preprocessor.pkl")
+
+        df=preprocessor.transform(df.iloc[:, :-1])
+        pred=model.predict(df)
+        df=pd.DataFrame(df)
+        df["predicted_label"]=pred
+        
+        table_html=df.to_html(classes="table table-striped")
+        return templates.TemplateResponse("result.html",{"request":request,"table_html":table_html,"pred":pred})
+    except Exception as e:
+        raise CustomException(e,sys)
+
+if __name__=="__main__":
+    try:
+        app_run(app,host="0.0.0",port=8080)
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -60,6 +60,7 @@ class DataTrasformation:
             save_numpy_array(self.data_transformation_config.transformed_train_file_path,train_array)
             save_numpy_array(self.data_transformation_config.transformed_test_file_path,test_array)
             save_object_pkl(self.data_transformation_config.transformed_object_file_path,preprocessor_obj)
+            save_object_pkl("final_model/final_preprocessor.pkl",preprocessor_obj)
             data_transformation_artifact=DataTransformationArtifact(transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,transformed_test_file_path=self.data_transformation_config.transformed_test_file_path,transformed_train_file_path=self.data_transformation_config.transformed_train_file_path)
             return data_transformation_artifact
 
